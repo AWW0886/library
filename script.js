1641ed9c14379e5a00abb00e51f0a9bdad064fec
Arnold W <arnoldwwong@gmail.com>
2023-08-19 22:55:06 +1000

    Add constructor

---
+++ script.js
@@ -0,0 +1,19 @@
+const myLibrary = [
+    {
+    title: 'The Hobbit',
+    author: 'JR Tolkien',
+    pages: 304,
+    read: 'yes',
+    }
+]
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBook() {
+
+}
