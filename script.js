6c86c5197d3f7456e7d5c140a2c220ac4c0318dc
Arnold W <arnoldwwong@gmail.com>
2023-08-20 18:58:10 +1000

    Add some DOM

--- script.js
+++ script.js
@@ -1,19 +1,48 @@
+const table = document.querySelector('.add-book');
+const addButton = document.querySelector('#add-button');
+const titleInput = document.querySelector('#title-input');
+const authorInput = document.querySelector('#author-input');
+const pagesInput = document.querySelector('#pages-input');
+
 const myLibrary = [
     {
     title: 'The Hobbit',
-    author: 'JR Tolkien',
+    author: 'JRR Tolkien',
     pages: 304,
-    read: 'yes',
+    genre: 'Fantasy/Sci-Fi',
+    read: 'Read',
     }
 ]
 
-function Book(title, author, pages, read) {
+function updateTable() {
+    myLibrary.forEach(book => {
+        let row = document.createElement('tr');
+        Object.keys(book).forEach(elem => {
+            let td = document.createElement('td');
+        })
+    })
+}
+
+function Book(title, author, pages, genre, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.genre = genre;
     this.read = read;
 }
 
 function addBook() {
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const genre = document.getElementById('genre').value;
+    const pages = document.getElementById('pages').value;
+    let read;
+    if (document.getElementById('read-input').checked == true) {
+        read = 'Read';
+    } else {
+        read = 'Not Read';
+    }
 
+    const newBook = new Book(title, author, genre, pages, read);
+    myLibrary.push(newBook);
 }
