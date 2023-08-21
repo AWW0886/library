eb25c2b2c3adc7b3738af834dd634f56d11b5710
Arnold W <arnoldwwong@gmail.com>
2023-08-22 00:08:00 +1000

    Add toggle for read status

--- script.js
+++ script.js
@@ -5,11 +5,13 @@ const clearButton = document.querySelector('.clear-button');
 const addButton = document.querySelector('#add-button');
 const titleInput = document.querySelector('#title-input');
 const authorInput = document.querySelector('#author-input');
-const notesInput = document.querySelector('.notes-input');
+const notesInput = document.querySelector('#notes-input');
 const pagesInput = document.querySelector('#pages-input');
 const genreSelect = document.querySelector('#genre-select');
-//const libraryCard = document.querySelector('.library');
+const libraryCard = document.querySelector('.library');
 const editButton = document.querySelector('.edit-button');
+const readIcon = document.querySelector('#read-icon');
+const testImage = document.querySelector('#test-image');
 
 const myLibrary = [
     {
@@ -17,8 +19,8 @@ const myLibrary = [
     author: 'JRR Tolkien',
     pages: '304',
     genre: 'Fantasy/Sci-Fi',
-    notes: '',
-    read: 'Read',
+    notes: 'Good Book',
+    read: "READ",
     }
 ]
 
@@ -37,7 +39,11 @@ function updateLibrary() {
         <p>${book.pages}</p>
         <p>${book.genre}</p>
         <p>${book.notes}</p>
-        <p>${book.read}</p>
+        <img src='img/text-box-outline.svg' alt='notes-icon' onclick='removeBook(${i})'>
+        <p class='read-status'>${book.read ? 'READ' : 'NOT READ YET'}</p>
+        <img id='read-icon' onclick='changeRead(${i})' src='img/book-check.svg' alt='read-icon'>
+        <button class='read-button' onclick='changeRead(${i})'>Read/Not Read
+        </button>
         <button class='edit-button' onclick='editBook(${i})'>Edit
         </button>
         <button class='remove-button' onclick='removeBook(${i})'>Remove
@@ -62,12 +68,12 @@ function addBookToLibrary() {
     let pages = document.getElementById('pages-input').value;
     let genre = document.getElementById('genre-select').value;
     let notes = document.getElementById('notes-input').value;
-    let read;
-    if (document.getElementById('read-input').checked == true) {
-        read = 'Read';
-    } else {
-        read = 'Not Read';
-    }
+    let read = document.getElementById('read-input').checked;
+    //if (document.getElementById('read-input').checked == true) {
+    //    read = 'Read';
+    //} else {
+    //    read = 'Not Read';
+    //}
 
     let newBook = new Book(title, author, pages, genre, notes, read);
     //console.log(newBook);
@@ -82,8 +88,9 @@ function clearForm() {
     authorInput.value = '';
     pagesInput.value = '';
     genreSelect.value = 0;
-    let textarea = document.querySelector('#notes-input');
-        textarea.value = '';
+    notesInput.value = '';
+//    let textarea = document.querySelector('#notes-input');
+//        textarea.value = '';
 }
 
 function removeBook(index) {
@@ -91,15 +98,45 @@ function removeBook(index) {
     updateLibrary();
 }
 
-function editBook(index) {
-    titleInput.value = index.title;
-    authorInput.value = index.author;
+function editBook(book) {
+    titleInput.value = book.title;
+    authorInput.value = book.author;
 }
 
-newBookButton.addEventListener('click', function() {
-    let addBookForm = document.querySelector('.input-form');
-    addBookForm.style.display = 'flex';
-})
+Book.prototype.changeRead = function() {
+    this.read = !this.read;
+}
+
+function changeRead(index) {
+    myLibrary[index].changeRead();
+    updateLibrary();
+}
+
+//function changeRead(index) {
+//    let readIcon = document.getElementById('read-icon');
+//    console.log('Hello');
+//    if (readIcon.getAttribute('src') === 'img/book-check.svg') {
+//        readIcon.setAttribute('src', 'img/book.svg');
+//    } else {
+//        readIcon.setAttribute('src', 'img/book-check.svg');
+//    }
+    //if (readIcon.src === '.img/book-check.svg') {
+    //    readIcon.src == 'img/book.svg';
+    //} else {
+    //    readIcon.src == '.img/book-check.svg';
+    //}
+//}
+
+
+//readIcon.addEventListener('click', function() {
+//    console.log('Hello');
+//})
+
+//newBookButton.addEventListener('click', function() {
+//    let addBookForm = document.querySelector('.input-form');
+//    addBookForm.style.display = 'flex';
+//    changeRead();
+//})
 
 clearButton.addEventListener('click', function() {
     clearForm();
